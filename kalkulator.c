90154c141b02a42e78a214afcb572cfd087e6f26
dradkowski <cyberdogpolska@gmail.com>
2018-10-11 18:26:07 +0200

    druga wersja

--- kalkulator.c
+++ kalkulator.c
@@ -1,1 +1,11 @@
 #include <stdio.h>
+
+int main(){
+    int a , b;
+    printf("podaj liczbe ");
+    scanf("%i", &a);
+    printf("podaj drugą liczbę ");
+    scanf("%i", &b);
+     
+    return 0;
+}
