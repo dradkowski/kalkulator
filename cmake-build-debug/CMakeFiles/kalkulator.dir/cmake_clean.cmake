462ad8dac478969e798eff5e18c5b63a22d5443e
dradkowski <cyberdogpolska@gmail.com>
2018-10-11 18:18:10 +0200

    pierwszy kalk

---
+++ cmake-build-debug/CMakeFiles/kalkulator.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kalkulator.dir/kalkulator.c.o"
+  "kalkulator.pdb"
+  "kalkulator"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/kalkulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
